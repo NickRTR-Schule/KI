7b435790896bf58508fc039a14757e50d2895177
Nick <95316598+NickRTR@users.noreply.github.com>
2023-10-23 09:43:57 +0200

    Print total gini

--- 01 Entscheidungsbaum/start.py
+++ 01 Entscheidungsbaum/start.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from func import *
 
-weekend = np.genfromtxt("weekend.csv", delimiter=",", dtype=str)
+weekend = np.genfromtxt("ape.csv", delimiter=",", dtype=str)
 
+print("Gesamt-Gini:", round(giniGesamt(weekend), 2))
 print("Bester Wurzelknoten des Entscheidungsbaumes:", bestGini(weekend))
