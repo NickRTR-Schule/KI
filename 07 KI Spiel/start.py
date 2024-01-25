7044111917aac5c3393abc330ced37992ce8b541
Nick <95316598+NickRTR@users.noreply.github.com>
2024-01-25 14:56:04 +0100

    Add collisions to game

--- 07 KI Spiel/start.py
+++ 07 KI Spiel/start.py
@@ -43,6 +43,9 @@ pipe2UppPosY = random.randint(-pipeHeight, 0)
 pipe2LowPosX = pipe2UppPosX
 pipe2LowPosY = pipe2UppPosY + pipeHeight + pipeGap
 
+active = 0
+gameOver = 0
+
 while(1):
     ''' EVENTS '''
     for event in pg.event.get():
@@ -51,50 +54,70 @@ while(1):
             sys.exit()
         if (event.type == pg.KEYDOWN): 
             if (event.key == pg.K_SPACE):
-                birdPosY -= 200
+                birdPosY -= 150
                 time = 0
 
     ''' HINTERGRUND '''
     screen.fill([255,255,255])
 
-    ''' PIPE 1'''
-    pipe1UppPosX -= 10
-    pipe1LowPosX = pipe1UppPosX
-    if (pipe1UppPosX < -pipeWidth):
-        pipe1UppPosX = width
-        pipe1UppPosY = random.randint(-pipeHeight, 0)
-        pipe1LowPosY = pipe1UppPosY + pipeHeight + pipeGap
+    if (gameOver == 0):
+        ''' PIPE 1'''
+        pipe1UppPosX -= 10
+        pipe1LowPosX = pipe1UppPosX
+        if (pipe1UppPosX < -pipeWidth):
+            pipe1UppPosX = width
+            pipe1UppPosY = random.randint(-pipeHeight, 0)
+            pipe1LowPosY = pipe1UppPosY + pipeHeight + pipeGap
 
-    ''' PIPE 2'''
-    pipe2UppPosX -= 10
-    pipe2LowPosX = pipe2UppPosX
-    if (pipe2UppPosX < -pipeWidth):
-        pipe2UppPosX = width
-        pipe2UppPosY = random.randint(-pipeHeight, 0)
-        pipe2LowPosY = pipe2UppPosY + pipeHeight + pipeGap
+        ''' PIPE 2'''
+        pipe2UppPosX -= 10
+        pipe2LowPosX = pipe2UppPosX
+        if (pipe2UppPosX < -pipeWidth):
+            pipe2UppPosX = width
+            pipe2UppPosY = random.randint(-pipeHeight, 0)
+            pipe2LowPosY = pipe2UppPosY + pipeHeight + pipeGap
 
-    # Pipe 1
-    pg.draw.rect(screen, [0,255,0], [pipe1UppPosX, pipe1UppPosY, pipeWidth, pipeHeight])
-    pg.draw.rect(screen, [0,255,0], [pipe1LowPosX, pipe1LowPosY, pipeWidth, pipeHeight])
+        # Pipe 1
+        pg.draw.rect(screen, [0,255,0], [pipe1UppPosX, pipe1UppPosY, pipeWidth, pipeHeight])
+        pg.draw.rect(screen, [0,255,0], [pipe1LowPosX, pipe1LowPosY, pipeWidth, pipeHeight])
 
-    # Pipe 2
-    pg.draw.rect(screen, [0,255,0], [pipe2UppPosX, pipe2UppPosY, pipeWidth, pipeHeight])
-    pg.draw.rect(screen, [0,255,0], [pipe2LowPosX, pipe2LowPosY, pipeWidth, pipeHeight])
+        # Pipe 2
+        pg.draw.rect(screen, [0,255,0], [pipe2UppPosX, pipe2UppPosY, pipeWidth, pipeHeight])
+        pg.draw.rect(screen, [0,255,0], [pipe2LowPosX, pipe2LowPosY, pipeWidth, pipeHeight])
 
-    ''' BIRD '''
-    pg.draw.circle(screen, [255,255,0], [birdPosX, birdPosY], birdRadius)
-    time += 0.10
-    birdPosY += time**2
-
-    ''' Text '''
-    printText('Flappy Bird', (400, 20), 40, [0,0,0])
-    printText('Punkte: ' + str(points), (20, 50), 25, [255, 255, 0])
+        ''' BIRD '''
+        pg.draw.circle(screen, [255,255,0], [birdPosX, birdPosY], birdRadius)
+        time += 0.1
+        birdPosY += time**2
 
     ''' KOLLISION '''
     # Pipe 1
     # Add a point if the bird passes the pipe in the gap without colliding
-    # if (birdPosX > pipe1UppPosX + pipeWidth and birdPosY < pipe1UppPosX + pipeWidth + 10):
-    #     points += 1
+    if (birdPosX >= pipe1UppPosX and birdPosX <= pipe1UppPosX + pipeWidth):
+        if active == 0:
+            if(birdPosY > pipe1UppPosY + pipeHeight and birdPosY < pipe1LowPosY):
+                points += 1
+                active = 1
+            else:
+                gameOver = 1
+
+    # Pipe 2
+    if (birdPosX >= pipe2UppPosX and birdPosX <= pipe2UppPosX + pipeWidth):
+        if active == 0:
+            if (birdPosY > pipe2UppPosY + pipeHeight and birdPosY < pipe2LowPosY):
+                points += 1
+                active = 1
+            else:
+                gameOver = 1
+
+    if (pipe1UppPosX < 0 or pipe2UppPosX < 0):
+        active = 0
+
+    ''' Text '''
+    printText('Flappy Bird', (400, 20), 40, [0,0,0])
+    printText('Punkte: ' + str(points), (20, 50), 25, [255, 255, 0])
+    if (gameOver == 1):
+        printText('Game Over', (400, 300), 40, [0,0,0])
 
     ''' ZEITEN '''
     FPSCLOCK.tick(FPS)
